b5b9943a4e3d141037636fcdf7121f80c69b1d40
jdel-18 <joshuadelves1@yahoo.com>
2023-05-01 14:40:58 -0500

    some updates

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # OOP-challenge-SVG-logo-maker
  
+## Description
+
+ This project was to create a SVG logo maker using inquirer and jest, and the purpose for this project is to ask the user a series of questions about what kind of logo they want to create and the code will create a simple logo. This code also has a test running in the background making sure its all working together to prevent a bug from happening.
+
+ ## Table of contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Contributing](#contributing)
+4. [Tests](#tests)
+
+## Installation
+
+Open the integrated terminal on the project, type in "npm i", wait for the download to complete.
+
+## Usage
+
+Using inquirer in the command line to answer some questions about creating a logo.
+
+Use "node index.js" to run code.
+
+## Contributing
+
+Inquirer - https://www.npmjs.com/package/inquirer
+Jest - https://www.npmjs.com/package/jest
+
--- index.js
+++ index.js
@@ -1,53 +1,60 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { default: Choice } = require('inquirer/lib/objects/choice');
-const { default: Choices } = require('inquirer/lib/objects/choices');
+const generateLogo = require('./utils/generateMarkdown')
 
 const questions = [
-    [
+    {
         type: 'input',
         name: 'initials',
-        message: 'Please put your initials for the logo you would like to create.'
-    ],
-    [
+        message: 'Please put your initials for the logo you would like to create. (Up to 3 max)'
+    },
+    {
         type:' list',
-        name: 'initials-color',
-        choices: [
-            'Blue',
-            'Orange',
-            'Yellow',
-            'Green',
-            'Purple',
-            'Pink',
-            'Black',
-            'White'
-        ],
-        message: 'Please choose a initials color.'
-    ],
-    [
+        message:'Please choose a color for the text.',
+        name: 'textColor',
+        choices: ['Blue', 'Orange', 'Yellow', 'Green']
+    },
+    {
         type:' list',
+        message:'Please choose a shape for the logo.',
         name: 'shape',
-        choices: [
-            'rectangle',
-            'circle',
-            'triangle'
-        ],
-        message: 'Please choose a shape.'
-    ],
-    [
+        choices: ['square', 'circle', 'triangle']
+    },
+    {
         type:' list',
-        name: 'shape-color',
-        choices: [
-            'Blue',
-            'Orange',
-            'Yellow',
-            'Green',
-            'Purple',
-            'Pink',
-            'Black',
-            'White'
-        ],
-        message: 'Please choose a shape color.'
-    ],
+        message:'Please choose a color for the shape.',
+        name: 'shapeColor',
+        choices: ['Blue', 'Orange', 'Yellow', 'Green']
+    },
 ];
 
+inquirer.prompt(questions).then((answer) => {
+    fs.writeFile
+})
+
+function generateSVGFile(filename, data) {
+    fs.writeFile(filename, data, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        } console.log(`Logo has been created and saved to ${filename}`);
+    });
+}
+
+function init() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'filename',
+            message: 'Please enter a filename for your logo (e.g. mylogo.svg):'
+        },
+        ...questions
+    ])
+    .then((data) => {
+        const logo = generateLogo(data);
+        generateSVGFile(data.filename, logo);
+    })
+    .catch((err)=> console.error(err));
+}
+
+init();
